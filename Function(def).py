e6ec77d4b2d7c963e4efe12c5f983c3991b9de03
abdulwagab <65432634+abdulwagab@users.noreply.github.com>
2020-10-06 09:01:53 +0900

    Add files via upload

---
+++ Function(def).py
@@ -0,0 +1,10 @@
+def clock(a, b):  #function definition
+    sum = a + b   #adding the arguments and store it to sum
+    return sum   #return the value
+clock1 = 5;  #Assigning values to variable
+clock2 = 3;  #Assigning values to variable
+print("clock value is: ", clock(clock1, clock2));  #print the statement
+
+
+
+

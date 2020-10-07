2cab3a836ccee24781c4319be2a093721ecb2735
abdulwagab <65432634+abdulwagab@users.noreply.github.com>
2020-10-07 09:19:48 +0900

    Add files via upload

---
+++ Operators1.py
@@ -0,0 +1,19 @@
+
+'''Relational Operators Example in Python'''
+def operator(x, y, z):  # Functions name and its arguments
+    a = 1  # Assign variables in Function
+    b = 2
+    c = 0
+    x = a or c and b  # Compare variables in Logical operators but not print
+
+    y = a and c or b # Compare variables in Logical operators but not print
+
+    z = a or b and c # Compare variables in Logical operators but not print
+    print(x, y, z) # Indirectly Assaign values to variables
+operator("x", "y", "z")  # Calling the Function
+
+
+
+
+
+

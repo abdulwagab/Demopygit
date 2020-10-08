b732409416605e7f488809a54c007de9476d8bb0
abdulwagab <65432634+abdulwagab@users.noreply.github.com>
2020-10-08 10:21:07 +0900

    Add files via upload

---
+++ function(def)2.py
@@ -0,0 +1,18 @@
+def addition(a, b):
+
+    a = 8
+    b = 2
+    c = []
+
+
+    if a < 7:
+        c = a + b
+        print("add : ", c)
+    else:
+        print("Decision control statement is going wrong so a - b is", a - b)
+
+
+
+addition(8, 2)
+
+

d8c3a1c6246bb60e4cb46e2eae4931880909db79
abdulwagab <65432634+abdulwagab@users.noreply.github.com>
2020-10-12 10:10:43 +0900

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+def pylist(x):
+    list = [5]  # it consists of 5 list
+    for list in range(4):  # using for loop to get the string values
+        x = input("Enter your names: ").split()  #variable assignment to the input
+        print(x) #print the values
+pylist('x')  # calling that function
